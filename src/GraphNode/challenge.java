8c0d46cedbf288c62987da508b4b9ad5ba4d4201
Dorian Holmes <doethemann@gmail.com>
2018-11-26 22:41:27 -0500

    testing

--- src/GraphNode/challenge.java
+++ src/GraphNode/challenge.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package GraphNode;
-
 /**
  *
  * @author Doetheman
  */
 public class challenge {
     
+   public static void main(String[] args) {
+       //Test input 
+         graph A = new graph("A"); 
+         graph B = new graph("B");
+         graph C = new graph("C");
+         graph D = new graph ("D");
+         graph E = new graph ("E");
+         graph F = new graph ("F");
+         graph G = new graph ("G");
+         graph H = new graph ("H");
+         graph I = new graph ("I");
+         graph J = new graph ("J");
+         GNode[] A_children;
+       A_children = new GNode[] { B, C, D};
+         A.setChildren(A_children);
+         GNode[] B_children;
+       B_children = new GNode[] { E, F, G};
+         B.setChildren(B_children);
+         GNode[] C_children;
+       C_children = new GNode[] { H, I, J};
+         C.setChildren(C_children);
+         
+         System.out.println(A.walkGraph(A));
+    }
 }
--- src/GraphNode/graph.java
+++ src/GraphNode/graph.java
@@ -5,6 +5,9 @@
  */
 package GraphNode;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  *
  * @author Doetheman
@@ -13,30 +16,93 @@ public class graph implements GNode{
     
     //Name of node
     String nodeName;
-    //Set name of node
-    public void setNodeName(String nodeN){
-        this.nodeName = nodeN;
+    //set constructor
+    graph(String a) {
+      this.nodeName = a;
+
     }
-    //Returns name of node
+     //Returns name of node
     @Override
     public String getName() {
         return this.nodeName;
     }
     
+    
+    //Array to store children of node
     GNode[] children;
-    public void setChildren(GNode[] children){
-        int length = children.length;
+    //Sets array for children 
+    public void setChildren(GNode[] setChildren){
+        int length = setChildren.length;
         this.children = new GNode[length];
-        
+        int childValue = 0;
+        for(GNode x: setChildren){
+            this.children[childValue] = x;
+            childValue++;
+        }
     }
+    //returns children array 
     @Override
     public GNode[] getChildren() {
         if(this.children == null){
         GNode[] emptyList = new GNode[0];
         return emptyList;
+         } else return this.children;
+    }
+    
+    
+    // ArrayList stores nodes in graph
+    // NOG is acronym for nodes on graph
+    private final ArrayList<GNode> NoG = new ArrayList<>();
+    
+    //Create method to check for recurring nodes by name?
+//    public boolean nodeCheck(GNode node){
+//        for(GNode name : node.getChildren()){
+//            if(node.getName().equals(name)){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+    
+    //Stores nodes and children in an Arraylist
+    public ArrayList walkGraph(GNode node){
+        //String name = node.getName();
+       //if(nodeCheck(node)){
+        //Adds node to list 
+        NoG.add(node);
+        for(GNode c : node.getChildren()){
+            walkGraph( c );
         }
+        
+        return NoG;
     }
-        public static void main(String[] args) {
-        // TODO code application logic here
+    //ArrayList inside an arraylist to store path to node
+    ArrayList<ArrayList<GNode>> pathNodeList = new ArrayList<>();
+    //Previous arraylist of nodes
+    ArrayList<GNode> prevNodeList = new ArrayList<>();
+    //Instantiating stack to get path in graph (DFS)
+    Stack<GNode> stack = new Stack<>();
+    //
+    public ArrayList paths(GNode node){
+        stack.push(node);
+        prevNodeList.add(node);
+        while(!stack.isEmpty()){
+            GNode current = stack.peek();
+            
+            GNode[] currentChildren = current.getChildren();
+            
+            for(GNode c : currentChildren){
+                paths(c);
+            }
+            stack.pop();
+            prevNodeList.remove(prevNodeList.size() - 1 );
+            
+            if (currentChildren.length == 0){
+                pathNodeList.add(new ArrayList<GNode>(prevNodeList));
+            }
+            break;
+        }
+        return pathNodeList;
     }
+
 }
